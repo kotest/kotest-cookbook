98f83f2bdbc94c070930951e08f6e7b9a68aaa36
AlexKuznetsov <alexander.kuznetsov@target.com>
2025-11-24 08:27:43 -0600

    AnsweringServiceWithInterface.kt

--- src/test/kotlin/io/kotest/cookbook/chapter2Fakery/section3VerifyCalled/VerifyingTestDoubleWasCalledTest.kt
+++ src/test/kotlin/io/kotest/cookbook/chapter2Fakery/section3VerifyCalled/VerifyingTestDoubleWasCalledTest.kt
@@ -2,6 +2,7 @@ package io.kotest.cookbook.chapter2Fakery.section3VerifyCalled
 
 import io.kotest.cookbook.chapter2Fakery.DecisionsEngineUsingFunction
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldNotContain
 
@@ -13,10 +14,14 @@ class VerifyingTestDoubleWasCalledTest: StringSpec() {
                 answer = { question : String ->
                     question.shouldNotContain("apple")
                     callCount++
+                    42
                 }
             )
             systemToTest.decide("Do oranges taste better than bananas?")
             callCount shouldBe 1
+        }
+        "test" {
+            listOf<Int>().shouldBeEmpty()
         }
     }
 }
